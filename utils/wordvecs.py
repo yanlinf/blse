c55ba3e139bc9a8af729a89e98519fc2f0cf1c33
evanfeng <evanfeng@pku.edu.cn>
2018-08-02 00:09:10 +0800

    fix bugs, changed to only binary format

--- utils/wordvecs.py
+++ utils/wordvecs.py
@@ -1,133 +1,109 @@
 import numpy as np
-import pickle
+import logging
 from scipy.spatial.distance import cosine
+
 
 class WordVecs(object):
     """
-    Helper class for importing word embeddings.
-    These embeddings can be either in
-    word2vec formats (txt or bin) or 
-    glove format.
+    Helper class for importing word embeddings in BINARY Word2Vec format.
 
-    If you already know the vocabulary you want to
-    import, you can pass it as a set or list
-    in the 'vocab' parameter.
+    Parameters
+    ----------
+    file: str
+        the binary embedding file containing word embbedings
+    vocab: list[str] / set[str], optional (default None)
+        if specified, out-of-vocabulary words won't be loaded
+    encoding: str
+        specify the encoding with which to decode the bytes
     """
 
-    def __init__(self, file, file_type='word2vec', vocab=None, encoding='utf8'):
-        self.file_type = file_type
-        self.vocab = vocab
+    def __init__(self, file, vocab=None, encoding='utf-8'):
+        self.vocab = set(vocab) if vocab else None
         self.encoding = encoding
-        (self.vocab_length, self.vector_size, self._matrix,
-         self._w2idx, self._idx2w) = self._read_vecs(file)
+        self.vocab_size, self.vec_dim, self._matrix, self._w2idx, self._idx2w = self._read_vecs(
+            file)
 
-    def __getitem__(self, y):
+    def __getitem__(self, word):
+        """
+        Returns the vector representation of a word.
+
+        word: str
+
+        Returns: np.array
+        """
         try:
-            return self._matrix[self._w2idx[y]]
+            return self._matrix[self._w2idx[word]]
         except KeyError:
-            raise KeyError
-        except IndexError:
-            raise IndexError
-        
+            raise KeyError('Word not in vocabulary')
 
     def _read_vecs(self, file):
-        """Assumes that the first line of the file is
-        the vocabulary length and vector dimension."""
+        """
+        Load word embeddings from the binary embedding file.
 
-        if self.file_type == 'word2vec':
-            txt = open(file, encoding=self.encoding).readlines()
-            vocab_length, vec_dim = [int(i) for i in txt[0].split()]
-            txt = txt[1:]
-        elif self.file_type == 'bin':
-            txt = open(file, 'rb')
-            header = txt.readline()
-            vocab_length, vec_dim = map(int, header.split())
-            binary_len = np.dtype('float32').itemsize * vec_dim
+        file: str
+        """
+        with open(file, 'rb') as fin:
+            header = fin.readline()
 
-        else:
-            txt = open(file).readlines()
-            vocab_length = len(txt)
-            vec_dim = len(txt[0].split()[1:])
+            vocab_size, vec_dim = map(int, header.split())
+            bytes_each_word = np.dtype('float32').itemsize * vec_dim
 
+            if self.vocab:
+                vocab_size = len(self.vocab)
 
-        if self.vocab:
-            emb_matrix = np.zeros((len(self.vocab), vec_dim))
-            vocab_length = len(self.vocab)
-        else:
-            emb_matrix = np.zeros((vocab_length, vec_dim))
-        w2idx = {}
+            emb_matrix = np.zeros((vocab_size, vec_dim), dtype='float32')
+            w2idx = {}
 
-        # Read a binary file
-        if self.file_type == 'bin':
-            for line in range(vocab_length):
-                word = []
+            for _ in range(vocab_size):
+                word = b''
                 while True:
-                    ch = txt.read(1)
-                    if ch == ' ':
-                        word = ''.join(word)
+                    ch = fin.read(1)
+                    if ch == b' ':
                         break
-                    if ch != '\n':
-                        word.append(ch)
-                # if you have vocabulary, you can only load these words
-                if self.vocab:
-                    if word in self.vocab:
-                        w2idx[word] = len(w2idx)
-                        emb_matrix[w2idx[word]] = np.fromstring(txt.read(binary_len), dtype='float32')  
-                    else:
-                        txt.read(binary_len)
+                    if ch != b'\n':
+                        word += ch
+                word = word.decode(self.encoding)
+
+                vec = np.fromstring(fin.read(bytes_each_word), dtype='float32')
+
+                if self.vocab and word not in self.vocab:
+                    continue
                 else:
                     w2idx[word] = len(w2idx)
-                    emb_matrix[w2idx[word]] = np.fromstring(txt.read(binary_len), dtype='float32')  
+                    emb_matrix[w2idx[word]] = vec
 
-        # Read a txt file
-        else:    
-            for item in txt:
-                if self.file_type == 'tang':            # tang separates with tabs
-                    split = item.strip().replace(',','.').split()
-                else:
-                    split = item.strip().split(' ')
-                try:
-                    word, vec = split[0], np.array(split[1:], dtype=float)
-
-                    # if you have vocabulary, only load these words
-                    if self.vocab:
-                        if word in self.vocab:
-                            w2idx[word] = len(w2idx)
-                            emb_matrix[w2idx[word]] = vec
-                        else:
-                            pass
-                    else:
-                        if len(vec) == vec_dim:
-                            w2idx[word] = len(w2idx)
-                            emb_matrix[w2idx[word]] = vec
-                        else:
-                            pass
-                except ValueError:
-                    pass
-
-            
-        idx2w = dict([(i, w) for w, i in w2idx.items()])
-
-        return vocab_length, vec_dim, emb_matrix, w2idx, idx2w
+        idx2w = {i: w for w, i in w2idx.items()}
+        return vocab_size, vec_dim, emb_matrix, w2idx, idx2w
 
     def most_similar(self, word, num_similar=5):
+        """
+        Returns the k most similar word to the given word.
+
+        word: str
+            specify the word to be compared among the vocab
+        num_similar: int, optional (default 5)
+            specify k
+
+        Returns: list[[int, str]]
+            [[dist1, word1], [dist2, word2], ..., [dist_k, word_k]]
+        """
         idx = self._w2idx[word]
-        y = list(range(self._matrix.shape[0]))
-        y.pop(idx)
-        most_similar = [(1,0)] * num_similar
-        for i in y:
-            dist = 0
-            dist = cosine(self._matrix[idx], self._matrix[i])
+        vec = self._matrix[idx]
+        most_similar = [(1, 0)] * num_similar
+        for i, cur_vec in enumerate(self._matrix):
+            if i == idx:
+                continue
+            dist = cosine(vec, cur_vec)
             if dist < most_similar[-1][0]:
                 most_similar.pop()
-                most_similar.append((dist,i))
+                most_similar.append((dist, i))
                 most_similar = sorted(most_similar)
-        most_similar = [(distance, self._idx2w[i]) for (distance, i) in most_similar]
-        return most_similar
+
+        return [[dist, self._idx2w[i]] for dist, i in most_similar]
 
     def normalize(self):
         norms = np.sqrt(np.sum(self._matrix**2, axis=1))
-        norms[norms == 0] = 1
+        norms[norms == .0] = 1
         self._matrix /= norms[:, np.newaxis]
 
     def mean_center(self):
