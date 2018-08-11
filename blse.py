db6e3293fdf19ec8eef9babb028e385e303ec031
evanfeng <evanfeng@pku.edu.cn>
2018-08-11 17:25:51 +0800

    rewrite

--- blse.py
+++ blse.py
@@ -9,100 +9,169 @@ import argparse
 from utils import utils
 
 
-def get_projection_loss(source_emb, target_emb, dictionary):
+class BLSE(object):
     """
-    Given the source language embedding, target language embedding and a bilingual dictionary,
-    compute the projection loss.
+    Bilingual sentiment embeddings.
+
+    Parameters
+    ----------
+    sess: tf.Session() object
+    saver: tf.train.Saver() object
+    src_emb: numpy.ndarray of shape (source_emb_size, vec_dim)
+    tgt_emb: numpy.ndarray of shape (source_emb_size, vec_dim)
+    dictionary: numpy.ndarray of shape (dictionary_size, 2)
     """
-    source_ids, target_ids = dictionary[:, 0], dictionary[:, 1]
 
-    proj_loss = tf.reduce_sum(tf.squared_difference(tf.nn.embedding_lookup(
-        source_emb, source_ids), tf.nn.embedding_lookup(target_emb, target_ids)))
-    return proj_loss
+    def __init__(self, sess, src_emb, tgt_emb, dictionary, savepath):
+        self.source_emb_obj = src_emb
+        self.target_emb_obj = tgt_emb
+        self.dict_obj = dictionary
+        self.sess = sess
+        self.savepath = savepath
+        self._build_graph()
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
 
+    def _build_graph(self):
+        """
+        Build the model.
+        """
 
-def softmax_layer(input):
-    """
-    compute $$ logits = input * P $$,
-    where the shape of logits is (None, n_classes)
-    doesn't perform softmax
-    """
-    with tf.variable_scope('softmax', reuse=tf.AUTO_REUSE):
-        P = tf.get_variable('P', (args.vec_dim, 4), dtype=tf.float32,
-                            initializer=tf.random_uniform_initializer(-1., 1.))
-    return tf.matmul(input, P)
+        def get_projection_loss(source_emb, target_emb, dictionary):
+            """
+            Given the source language embedding, target language embedding and a bilingual dictionary,
+            compute the projection loss.
+            """
+            source_ids, target_ids = dictionary[:, 0], dictionary[:, 1]
 
+            proj_loss = tf.reduce_sum(tf.squared_difference(tf.nn.embedding_lookup(
+                source_emb, source_ids), tf.nn.embedding_lookup(target_emb, target_ids)))
+            return proj_loss
 
-def get_classification_loss_pred(emb, X, y):
-    """
-    Given the language embedding and the classification dataset,
-    compute the classification loss.
-    """
-    X = tf.nn.embedding_lookup(emb, X)  # shape: (None, 256, 300)
-    X = tf.reduce_mean(X, axis=1)  # shape: (None, 300)
-    # for sent in X_id:
-    #     word_list = [tf.nn.embedding_lookup(emb, word_id) for word_id in sent]
-    #     sent_vec = tf.reduce_mean(tf.stack(word_list, axis=0), axis=0)
-    #     X.append(sent_vec)
-    # X = tf.stack(X, axis=0)
+        def softmax_layer(input):
+            """
+            compute $$ logits = input * P $$,
+            where the shape of logits is (None, n_classes)
+            doesn't perform softmax
+            """
+            with tf.variable_scope('softmax', reuse=tf.AUTO_REUSE):
+                P = tf.get_variable('P', (args.vec_dim, 4), dtype=tf.float32,
+                                    initializer=tf.random_uniform_initializer(-1., 1.))
+                b = tf.get_variable('b', (4,), dtype=tf.float32)
+            return tf.matmul(input, P) + b
 
-    hypothesis = softmax_layer(X)
-    pred = tf.argmax(hypothesis, axis=1, output_type=tf.int32)
-    classification_loss = tf.losses.softmax_cross_entropy(
-        tf.one_hot(y, 4), hypothesis)
-    return classification_loss, pred
+        def get_projected_embeddings(source_original_emb, target_original_emb):
+            """
+            Given the original embeddings, calculate the projected word embeddings.
+            """
+            with tf.variable_scope('projection', reuse=tf.AUTO_REUSE):
+                W_source = tf.get_variable(
+                    'W_source', (args.vec_dim, args.vec_dim), dtype=tf.float32, initializer=tf.random_uniform_initializer(-1., 1.))
+                W_target = tf.get_variable(
+                    'W_target', (args.vec_dim, args.vec_dim), dtype=tf.float32, initializer=tf.random_uniform_initializer(-1., 1.))
+            source_emb = tf.matmul(source_original_emb,
+                                   W_source, name='map_source')
+            target_emb = tf.matmul(target_original_emb,
+                                   W_target, name='map_target')
+            return source_emb, target_emb
 
+        self.source_original_emb = tf.placeholder(
+            tf.float32, shape=(None, args.vec_dim))
+        self.target_original_emb = tf.placeholder(
+            tf.float32, shape=(None, args.vec_dim))
+        self.corpus = tf.placeholder(tf.int32, shape=(None, 256))
+        self.labels = tf.placeholder(tf.int32, shape=(None,))
+        self.dictionary = tf.placeholder(tf.int32, shape=(None, 2))
 
-def get_full_loss_pred(source_emb, target_emb, dictionary, X_id, y):
-    """
-    Compute the loss and classification prediction.
-    $$ full_loss = alpha * classification_loss + (1 - alpha) * projection_loss $$
+        source_emb, target_emb = get_projected_embeddings(
+            source_original_emb, target_original_emb)
 
-    source_emb: tensor of shape (vocabsize, vec_dim)
-        projected word embedding of the source language
-    target_emb: tensor of shape (vocabsize, vec_dim)
-        projected word embedding of the target language    
-    dictionary: tensor of shape (dictsize, 2)
-        the bilingual dictionary used to compute the projection loss, each item containing
-        two indices.
-    X_id: list[list[int]]
-        examples in the sentiment dataset
-    y: tensor of shape (None,)
-        labels of the dataset
-    """
-    proj_loss = get_projection_loss(source_emb, target_emb, dictionary)
-    classification_loss, pred = get_classification_loss_pred(
-        source_emb, X_id, y)
-    full_loss = args.alpha * classification_loss + (1 - args.alpha) * proj_loss
-    return full_loss, classification_loss, proj_loss, pred
+        # compute projection loss
+        self.proj_loss = get_projection_loss(
+            source_emb, target_emb, self.dictionary)
 
+        # compute classification loss
+        sents = tf.reduce_mean(tf.nn.embedding_lookup(
+            source_emb, self.corpus), axis=1)  # shape: (None, 300)
+        hypothesis = softmax_layer(sents)
+        self.classification_loss = tf.losses.softmax_cross_entropy(
+            tf.one_hot(y, 4), hypothesis)
 
-# def get_prediction(emb, X_id):
-#     X = []
-#     for sent in X_id:
-#         word_list = [tf.nn.embedding_lookup(emb, word_id) for word_id in sent]
-#         sent_vec = tf.reduce_mean(tf.stack(word_list, axis=0), axis=0)
-#         X.append(sent_vec)
-#     X = tf.stack(X, axis=0)
-#     pred = tf.argmax(softmax_layer(X), axis=1)
-#     return pred
+        # compute full loss
+        self.loss = args.alpha * self.classification_loss + \
+            (1 - args.alpha) * self.proj_loss
 
+        # compute accuracy counts
+        self.pred = tf.argmax(hypothesis, axis=1, output_type=tf.int32)
+        self.acc = tf.reduce_mean(tf.to_float(
+            tf.equal(pred, labels)))  # tensor
 
-def get_projected_embeddings(source_original_emb, target_original_emb):
-    """
-    Given the original embeddings, calculate the projected word embeddings.
-    """
-    with tf.variable_scope('projection', reuse=tf.AUTO_REUSE):
-        W_source = tf.get_variable(
-            'W_source', (args.vec_dim, args.vec_dim), dtype=tf.float32, initializer=tf.random_uniform_initializer(-1., 1.))
-        W_target = tf.get_variable(
-            'W_target', (args.vec_dim, args.vec_dim), dtype=tf.float32, initializer=tf.random_uniform_initializer(-1., 1.))
-    source_emb = tf.matmul(source_original_emb, W_source, name='map_source')
-    target_emb = tf.matmul(target_original_emb, W_target, name='map_target')
-    return source_emb, target_emb
+        self.global_step = tf.Variable(0,
+                                       dtype=tf.int32,
+                                       trainable=False,
+                                       name='global_step')
+
+        self.optimizer = tf.train.AdamOptimizer(
+            args.learning_rate).minimize(self.loss, global_step=self.global_step)
+
+    def save(self, path):
+        """
+        Save the model to the given path.
+        """
+        self.saver.save(self.sess, path, global_step=self.global_step)
+
+    def fit(self, train_x, train_y):
+        """
+        train the model.
+        """
+        nsample = len(train_x)
+        nbatch = nsample // args.batch_size
+        for epoch in range(args.epochs):
+            closs, ploss, loss, acc = 0., 0.
+            for index, offset in enumerate(range(0, nsample, args.batch_size)):
+                xs = train_x[offset:offset + args.batch_size]
+                ys = train_y[offset:offset + args.batch_size]
+                feed_dict = {
+                    self.source_original_emb: self.source_emb_obj,
+                    self.target_original_emb: self.target_emb_obj,
+                    self.dictionary: self.dict_obj,
+                    self.corpus: xs,
+                    self.labels: ys,
+                }
+                closs_, ploss_, loss_, acc_, _, self.sess.run(
+                    [self.classification_loss, self.proj_loss, self.loss, self.acc, self.optimizer], feed_dict=feed_dict)
+
+                closs += closs_
+                ploss += ploss_
+                loss += loss_
+                acc += acc_
+
+            closs, ploss, loss, acc = closs / nbatch, ploss / \
+                nbatch, loss / nbatch, acc / nbatch
+            print('epoch: %d  loss: %.4f  class_loss: %.4f  proj_loss: %.4f  train_acc: %.2f=' %
+                  (epoch, loss, closs, ploss, acc))
+            if (epoch + 1) % 10 == 0:
+                self.save(self.savepath)
+
+    def evaluate(self, test_x, test_y):
+        """
+        Compute the accuracy given the test examples.
+        """
+        feed_dict = {
+            self.source_original_emb: self.source_emb_obj,
+            self.target_original_emb: self.target_emb_obj,
+            self.dictionary: self.dict_obj,
+            self.corpus: test_x,
+            self.labels: test_y,
+        }
+        acc = self.sess.run(self.acc, feed_dict=feed_dict)
+        print('test accuracy: %.4f' % acc)
 
 
 def load_data():
+    """
+    Return the data in numpy arrays.
+    """
     source_wordvec = utils.WordVecs(args.source_embedding)
     target_wordvec = utils.WordVecs(args.target_embedding)
     args.vec_dim = source_wordvec.vec_dim
@@ -124,56 +193,13 @@ def load_data():
     return source_wordvec.embedding, target_wordvec.embedding, dict_obj, train_x, train_y, test_x, test_y
 
 
-def get_input_layer():
-    source_original_emb = tf.placeholder(
-        tf.float32, shape=(None, args.vec_dim))
-    target_original_emb = tf.placeholder(
-        tf.float32, shape=(None, args.vec_dim))
-    corpus = tf.placeholder(tf.int32, shape=(None, 256))
-    labels = tf.placeholder(tf.int32, shape=(None,))
-    dictionary = tf.placeholder(tf.int32, shape=(None, 2))
-    return source_original_emb, target_original_emb, corpus, labels, dictionary
-
-
 def main():
-    source_emb_obj, target_emb_obj, dict_obj, train_x, train_y, test_x, test_y = load_data()  # numpy arrays
+    source_emb_obj, target_emb_obj, dict_obj, train_x, train_y, test_x, test_y = load_data()  # numpy array
 
-    source_original_emb, target_original_emb, corpus, labels, dictionary = get_input_layer()  # tensors
-
-    source_emb, target_emb = get_projected_embeddings(
-        source_original_emb, target_original_emb)
-
-    global_step = tf.Variable(0,
-                              dtype=tf.int32,
-                              trainable=False,
-                              name='global_step')
-    loss, class_loss, proj_loss, pred = get_full_loss_pred(source_emb, target_emb,
-                                                           dictionary, corpus, labels)
-
-    acc = tf.reduce_mean(tf.to_float(tf.equal(pred, labels)))
-    optimizer = tf.train.AdamOptimizer(
-        args.learning_rate).minimize(loss, global_step=global_step)
-
-    saver = tf.train.Saver()
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(args.epochs):
-            loss_, class_loss_, proj_loss_, _, train_acc_ = sess.run([loss, class_loss, proj_loss, optimizer, acc], feed_dict={
-                source_original_emb: source_emb_obj,
-                target_original_emb: target_emb_obj,
-                corpus: train_x,
-                labels: train_y,
-                dictionary: dict_obj, })
-
-            acc_ = sess.run(acc, feed_dict={
-                source_original_emb: source_emb_obj,
-                corpus: test_x,
-                labels: test_y})
-            print('epoch: %d  loss: %.4f  class_loss: %.4f  proj_loss_: %.4f  train_acc: %.2f  test_acc: %.2f' %
-                  (epoch, loss_, class_loss_, proj_loss_, train_acc_, acc_))
-
-            if (epoch + 1) % 10 == 0:
-                saver.save(sess, './checkpoints/blse', global_step=global_step)
+        model = BLSE(sess, source_emb_obj, target_emb_obj, dict_obj)
+        model.fit(train_x, train_y)
+        model.evaluate(test_x, test_y)
 
 
 if __name__ == '__main__':
@@ -219,5 +245,8 @@ if __name__ == '__main__':
     parser.add_argument('--debug',
                         help='print debug info',
                         action='store_true')
+    parser.add_argument('--save_path',
+                        help='the dictionary to store the trained model',
+                        default='./checkpoints/')
     args = parser.parse_args()
     main()
