01af15de125467aadc2ddbe63350008acfcf69a8
evanfeng <evanfeng@pku.edu.cn>
2018-08-25 15:31:31 +0800

    experiment

---
+++ run.sh
@@ -0,0 +1,7 @@
+nohup python -u blse.py -e 10 -lr 0.01 &
+nohup python -u blse.py -e 10 -lr 0.01 --alpha 0.0 &
+nohup python -u blse.py -e 10 -lr 0.03 --alpha 0.0 &
+nohup python -u blse.py -e 10 -lr 0.1 --alpha 0.0 &
+nohup python -u blse.py -e 10 -lr 0.01 --alpha 1.0 &
+nohup python -u blse.py -e 10 -lr 0.03 --alpha 1.0 &
+nohup python -u blse.py -e 10 -lr 0.1 --alpha 1.0 &
