b0dcb1999e622ab4b55faf25550d694dbf72d170
evanfeng <evanfeng@pku.edu.cn>
2018-08-25 18:46:53 +0800

    search alpha

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python -u blse.py -e 10 -lr 0.01
-python -u blse.py -e 10 -lr 0.01 --alpha 0.0
-python -u blse.py -e 10 -lr 0.03 --alpha 0.0
-python -u blse.py -e 10 -lr 0.1 --alpha 0.0
-python -u blse.py -e 10 -lr 0.01 --alpha 1.0
-python -u blse.py -e 10 -lr 0.03 --alpha 1.0
-python -u blse.py -e 10 -lr 0.1 --alpha 1.0
+python -u blse.py -e 20 -lr 0.01 --alpha 0.0 --debug
+python -u blse.py -e 20 -lr 0.01 --alpha 0.00001 --debug
+python -u blse.py -e 20 -lr 0.01 --alpha 0.0001 --debug
+python -u blse.py -e 20 -lr 0.01 --alpha 0.001 --debug
+python -u blse.py -e 20 -lr 0.01 --alpha 0.01 --debug
+python -u blse.py -e 20 -lr 0.01 --alpha 0.1 --debug
+python -u blse.py -e 20 -lr 0.01 --alpha 1.0 --debug
