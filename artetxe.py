f9f4f5ce7091f8c15b445520d7653fd8b0d94b08
evanfeng <evanfeng@pku.edu.cn>
2018-09-10 17:28:49 +0800

    fix bug

--- artetxe.py
+++ artetxe.py
@@ -1,7 +1,7 @@
-import tensorflow as tf
 import numpy as np
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.metrics import make_scorer, f1_score
 import argparse
 from utils import utils
 from multiprocessing import cpu_count
@@ -9,18 +9,46 @@ import pickle
 import logging
 
 
-def get_W_target(source_emb, target_emb, dict_obj):
+def get_W_target(source_emb, target_emb, dict_obj, orthogonal):
     """
     source_emb: np.ndarray of shape (source_vocab_size, vec_dim)
     target_emb: np.ndarray of shape (target_vocab_size, vec_dim)
     dict_obj: np.ndarray of shape (dict_size, 2)
+    orthogonal: bool
 
     Returns: np.ndarray of shape (vec_dim, vec_dim)
     """
     X_source = source_emb[dict_obj[:, 0]]  # shape (dict_size, vec_dim)
     X_target = target_emb[dict_obj[:, 1]]  # shape (dict_size, vec_dim)
-    W_target = np.matmul(np.linalg.pinv(X_target), X_source)
+
+    if orthogonal:
+        u, s, vt = np.linalg.svd(np.dot(X_source.T, X_target))
+        W_target = np.dot(vt.T, u.T)
+    else:
+        W_target = np.matmul(np.linalg.pinv(X_target), X_source)
+
     return W_target
+
+
+def get_W_source(source_emb, target_emb, dict_obj, orthogonal):
+    """
+    source_emb: np.ndarray of shape (source_vocab_size, vec_dim)
+    target_emb: np.ndarray of shape (target_vocab_size, vec_dim)
+    dict_obj: np.ndarray of shape (dict_size, 2)
+    orthogonal: bool
+
+    Returns: np.ndarray of shape (vec_dim, vec_dim)
+    """
+    X_source = source_emb[dict_obj[:, 0]]  # shape (dict_size, vec_dim)
+    X_target = target_emb[dict_obj[:, 1]]  # shape (dict_size, vec_dim)
+
+    if orthogonal:
+        u, s, vt = np.linalg.svd(np.dot(X_target.T, X_source))
+        W_source = np.dot(vt.T, u.T)
+    else:
+        W_source = np.matmul(np.linalg.pinv(X_source), X_target)
+
+    return W_source
 
 
 def cal_proj_loss(source_emb, target_emb, dict_obj):
@@ -29,73 +57,78 @@ def cal_proj_loss(source_emb, target_emb, dict_obj):
     return np.sum(np.square(X_source - X_target))
 
 
+def lookup_and_shuffle(X, y, emb, binary=False):
+    X_new = np.zeros((len(X), emb.shape[1]))
+    for i, line in enumerate(X):
+        if len(line) == 0:
+            logging.warning('ZERO LENGTH EXAMPLE')
+            continue
+        X_new[i] = np.mean(emb[line], axis=0)
+    X = X_new
+
+    perm = np.random.permutation(X.shape[0])
+    X, y = X[perm], y[perm]
+    if binary:
+        y = (y >= 2).astype(np.int32)
+    return X, y
+
+
 def main(args):
     logging.info(str(args))
 
-    if args.load:
-        with open('./tmp/pickled_%dclass.bin' % (2 if args.binary else 4), 'rb') as fin:
-            source_wordvec, target_wordvec, dict_obj, train_x, train_y, dev_x, dev_y, test_x, test_y = pickle.load(
-                fin)
+    # load word embedding
+    source_wordvec = utils.WordVecs(args.source_embedding)
+    target_wordvec = utils.WordVecs(args.target_embedding)
+    if args.normalize:
+        source_wordvec.mean_center().normalize()
+        target_wordvec.mean_center().normalize()
+
+    # load bilingual lexicon
+    dict_obj = utils.BilingualDict(args.dictionary).filter(
+        lambda x: x[0] != '-').get_indexed_dictionary(source_wordvec, target_wordvec)
+
+    source_dataset = utils.SentimentDataset(
+        args.source_dataset).to_index(source_wordvec)
+    target_dataset = utils.SentimentDataset(
+        args.target_dataset).to_index(target_wordvec)
+
+    # create bilingual embedding
+    if args.project_source:
+        proj_source_emb = np.matmul(source_wordvec.embedding, get_W_source(
+            source_wordvec.embedding, target_wordvec.embedding, dict_obj, args.orthogonal))
+        proj_target_emb = target_wordvec.embedding
     else:
-        source_wordvec = utils.WordVecs(args.source_embedding)
-        target_wordvec = utils.WordVecs(args.target_embedding)
+        proj_source_emb = source_wordvec.embedding
+        proj_target_emb = np.matmul(target_wordvec.embedding, get_W_target(
+            source_wordvec.embedding, target_wordvec.embedding, dict_obj, args.orthogonal))
 
-        source_pad_id = source_wordvec.add_word('<PAD>', np.zeros(300))
-        target_pad_id = target_wordvec.add_word('<PAD>', np.zeros(300))
+    logging.info('projection loss before projection: %.2f' % cal_proj_loss(
+        source_wordvec.embedding, target_wordvec.embedding, dict_obj))
+    logging.info('projection loss after projection: %.2f' % cal_proj_loss(
+        proj_source_emb, proj_target_emb, dict_obj))
 
-        dict_obj = utils.BilingualDict(args.dictionary).filter(
-            lambda x: x[0] != '-').get_indexed_dictionary(source_wordvec, target_wordvec)
+    # embedding lookup and prepare traning data
+    train_x = source_dataset.train[
+        0] + source_dataset.dev[0] + source_dataset.test[0]
+    train_y = np.concatenate(
+        (source_dataset.train[1], source_dataset.dev[1], source_dataset.test[1]), axis=0)
 
-        source_dataset = utils.SentimentDataset(
-            args.source_dataset).to_index(source_wordvec)
-        target_dataset = utils.SentimentDataset(
-            args.target_dataset).to_index(target_wordvec)
+    train_x, train_y = lookup_and_shuffle(
+        train_x, train_y, proj_source_emb, args.binary)
+    test_x, test_y = lookup_and_shuffle(*target_dataset.train, proj_target_emb, args.binary)
 
-        def pad_and_shuffle(X, y, pad_id, emb, binary=False):
-            X = tf.keras.preprocessing.sequence.pad_sequences(
-                X, maxlen=64, value=pad_id)
-            perm = np.random.permutation(X.shape[0])
-            X, y = X[perm], y[perm]
-            if binary:
-                y = (y >= 2).astype(np.int32)
-            print(emb[:5])
-            print(emb[X].shape)
-            X = np.sum(emb[X], axis=1)
-            return X, y
-
-        W_target = get_W_target(source_wordvec.embedding,
-                                target_wordvec.embedding, dict_obj)
-        proj_target_emb = np.matmul(target_wordvec.embedding, W_target)
-
-        logging.info('projection loss before projection: %.2f' % cal_proj_loss(source_wordvec.embedding, target_wordvec.embedding, dict_obj))
-        logging.info('projection loss after projection: %.2f' % cal_proj_loss(source_wordvec.embedding, proj_target_emb, dict_obj))
-
-        train_x, train_y = pad_and_shuffle(*source_dataset.train, source_pad_id, source_wordvec.embedding, args.binary)
-        dev_x, dev_y = pad_and_shuffle(*target_dataset.test, target_pad_id, proj_target_emb, args.binary)
-        test_x, test_y = pad_and_shuffle(*target_dataset.train, target_pad_id, proj_target_emb, args.binary)
-
-        # with open('./tmp/pickled_%dclass.bin' % (2 if args.binary else 4), 'wb') as fout:
-        #     pickle.dump((source_wordvec, target_wordvec, dict_obj, train_x,
-        # train_y, dev_x, dev_y, test_x, test_y), fout, protocol=4)
-
+    # train linear SVM classifier and tune parameter C
     param_grid = {
-        'C': [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30],
+        'C': [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100],
     }
-    cv_split = PredefinedSplit(np.concatenate(
-        (np.full(train_x.shape[0], -1), np.full(dev_x.shape[0], 0)), axis=0))
     svc = svm.LinearSVC()
-
     clf = GridSearchCV(svc, param_grid, scoring='f1_macro',
-                       cv=cv_split, n_jobs=cpu_count(), verbose=3)
+                       cv=None, n_jobs=cpu_count(), verbose=3)
 
-    X = np.concatenate((train_x, dev_x), axis=0)
-    y = np.concatenate((train_y, dev_y), axis=0)
-    clf.fit(X, y)
+    clf.fit(train_x, train_y)
 
-    print('Test score: %.4f' % clf.score(test_x, test_y))
-    print('Scorer:', clf.scorer_)
-    print('Best params', clf.best_params_)
-    print('CV results:', clf.cv_results_)
+    print('Test F1_macro: %.4f' % clf.score(test_x, test_y))
+    print('Best params: ', clf.best_params_)
 
 
 if __name__ == '__main__':
@@ -117,20 +150,22 @@ if __name__ == '__main__':
                         default='./emb/es.bin')
     parser.add_argument('-d', '--dictionary',
                         help='bilingual dictionary of source and target language (default: ./lexicons/bingliu/en-es.txt',
-                        default='./lexicons/bingliu/en-eu.txt')
+                        default='./lexicons/bingliu/en-es.txt')
     parser.add_argument('-sd', '--source_dataset',
                         help='sentiment dataset of the source language',
                         default='./datasets/en/opener_sents/')
     parser.add_argument('-td', '--target_dataset',
                         help='sentiment dataset of the target language',
                         default='./datasets/es/opener_sents/')
-    parser.add_argument('-vd', '--vector_dim',
-                        help='dimension of each word vector (default: 300)',
-                        default=300,
-                        type=int)
-    parser.add_argument('--load',
-                        action='store_true',
-                        help='load from pickled object')
+    parser.add_argument('--project_source',
+                        help='project source embedding (default: project target)',
+                        action='store_true')
+    parser.add_argument('--normalize',
+                        help='mean center and normalize word vectors',
+                        action='store_true')
+    parser.add_argument('--orthogonal',
+                        help='apply orthogonal restriction to the projection matrix',
+                        action='store_true')
     parser.add_argument('--debug',
                         help='print debug info',
                         action='store_const',
