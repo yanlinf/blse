c6bc0b7ce1e6b250c614fbf8bdb852dcb287e6dc
evanfeng <evanfeng@pku.edu.cn>
2018-09-18 15:53:38 +0800

    add cupy

--- boostrap.py
+++ boostrap.py
@@ -2,24 +2,46 @@ import numpy as np
 import argparse
 import pickle
 import logging
+import sys
+import re
 from utils import utils
 
-
-BATCH_SIZE = 500
+try:
+    import cupy
+except ImportError:
+    cupy = None
 
 def main(args):
     logging.info(str(args))
 
+    if args.cuda:
+        if cupy is None:
+            print('Install cupy for cuda support')
+        sys.exit(-1)
+        xp = cupy
+    else:
+        xp = np
+
     source_wordvecs = utils.WordVecs(args.source_embedding).mean_center().normalize()
     target_wordvecs = utils.WordVecs(args.target_embedding).mean_center().normalize()
-    init_dict = utils.BilingualDict(args.dictionary).get_indexed_dictionary(
-        source_wordvecs, target_wordvecs)
-    gold_dict = utils.BilingualDict(args.gold_dictionary).get_indexed_dictionary(
-        source_wordvecs, target_wordvecs)
+    gold_dict = utils.BilingualDict(args.gold_dictionary).get_indexed_dictionary(source_wordvecs, target_wordvecs)
+    if args.init_num:
+        num_regex = re.compile('$[0-9]+^')
+        src_nums = {w for w in source_wordvecs.vocab if num_regex.match(w) is not None}
+        trg_nums = {w for w in target_wordvecs.vocab if num_regex.match(w) is not None}
+        common = src_nums & trg_nums
+        init_dict = xp.array([[source_wordvecs.word2index(w), target_wordvecs.word2index(w)] for w in common], dtype=xp.int32)
+    else:
+        init_dict = utils.BilingualDict(args.dictionary).get_indexed_dictionary(source_wordvecs, target_wordvecs)
+    print('init_dict_size: %d' % init_dict.shape[0])
 
     vocab_size = source_wordvecs.embedding.shape[0]
-    logging.info('vocab_size: %d' % vocab_size)
+    print('vocab_size: %d' % vocab_size)
     curr_dict = init_dict
+    cos_sims = xp.empty((args.batch_size, target_emb.shape[0]))
+    source_emb = source_wordvecs.embedding
+    target_emb = xp.empty((target_emb.shape[0], args.vector_dim))
+
     for epoch in range(args.epochs):
         X_source = source_wordvecs.embedding[curr_dict[:, 0]]
         X_target = target_wordvecs.embedding[curr_dict[:, 1]]
@@ -28,22 +50,20 @@ def main(args):
             with open(args.W_target, 'rb') as fin:
                 W_target = pickle.load(fin)
         else:
-            u, s, vt = np.linalg.svd(np.dot(X_source.T, X_target))
-            W_target = np.dot(vt.T, u.T)
-
-        source_emb = source_wordvecs.embedding
-        target_emb = np.dot(target_wordvecs.embedding, W_target)
+            u, s, vt = xp.linalg.svd(xp.dot(X_source.T, X_target))
+            W_target = xp.dot(vt.T, u.T)
+        xp.dot(target_wordvecs.embedding, W_target, out=target_emb)
 
         
-        curr_dict = np.zeros((vocab_size, 2), dtype=np.int32)
-        curr_dict[:, 0] = np.arange(vocab_size)
-        for i in range(0, vocab_size, BATCH_SIZE):
+        curr_dict = xp.zeros((vocab_size, 2), dtype=xp.int32)
+        curr_dict[:, 0] = xp.arange(vocab_size)
+        for i in range(0, vocab_size, args.batch_size):
             print(i)
-            similarities = np.dot(source_emb[i:i + BATCH_SIZE], target_emb.T)  # shape (BATCH_SIZE, TARGET_VOCAB_SIZE)
-            closest = np.argmax(similarities, axis=1)
-            curr_dict[i:i + BATCH_SIZE, 1] = closest
+            j = i + args.batch_size
+            xp.dot(source_emb[i:j], target_emb.T, out=cos_sims)  # shape (BATCH_SIZE, TARGET_VOCAB_SIZE)
+            xp.argmax(cos_sims, axis=1, out=curr_dict[i:j, 1])
 
-        accuracy = np.mean((curr_dict[gold_dict[:, 0]][:, 1] == gold_dict[:, 1]).astype(np.int32))
+        accuracy = xp.mean((curr_dict[gold_dict[:, 0]][:, 1] == gold_dict[:, 1]).astype(xp.int32))
         logging.info('epoch: %d   accuracy: %.4f   dict_size: %d' % (epoch, accuracy, curr_dict.shape[0]))
 
     with open(args.save_path, 'wb') as fout:
@@ -64,9 +84,7 @@ if __name__ == '__main__':
     parser.add_argument('-te', '--target_embedding',
                         help='monolingual word embedding of the target language (default: ./emb/es.bin)',
                         default='./emb/es.bin')
-    parser.add_argument('-d', '--dictionary',
-                        help='bilingual dictionary for learning bilingual mapping (default: ./lexicons/bingliu/en-es.txt)',
-                        default='./lexicons/bingliu/en-es.txt')
+    
     parser.add_argument('-gd', '--gold_dictionary',
                         help='gold bilingual dictionary for evaluation(default: ./lexicons/apertium/en-es.txt)',
                         default='./lexicons/apertium/en-es.txt')
@@ -82,6 +100,10 @@ if __name__ == '__main__':
                         help='restore W_target from a file',
                         type=str,
                         default='')
+    parser.add_argument('-bs', '--batch_size',
+                        help='training batch size (default: 10000)',
+                        default=10000,
+                        type=int)
     parser.add_argument('--debug',
                         help='print debug info',
                         action='store_const',
@@ -91,6 +113,15 @@ if __name__ == '__main__':
     parser.add_argument('--save_path',
                         help='file to save the learned W_target',
                         default='./checkpoints/boostrap.txt')
+
+    dict_group = parser.add_mutually_exclusive_group(required=True)
+    dict_group.add_argument('-d', '--dictionary',
+                            help='bilingual dictionary for learning bilingual mapping (default: ./lexicons/bingliu/en-es.txt)',
+                            default='./lexicons/bingliu/en-es.txt')
+    dict_group.add_argument('--init_num', 
+                            action='store_true',
+                            help='use numerals as initial dictionary')
+
     args = parser.parse_args()
 
     logging.basicConfig(level=args.loglevel,
