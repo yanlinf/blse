e3ce75917566becd7daaf63cd33927d3dffdceb3
evanfeng <evanfeng@pku.edu.cn>
2018-09-27 13:55:48 +0800

    add summary, fix nan bug

--- bi_lr.py
+++ bi_lr.py
@@ -6,10 +6,15 @@ from utils import utils
 import logging
 
 
+def DEBUG(s):
+    logging.debug('[DEBUG] ' + str(s))
+
+
 class BiSentiLR(object):
 
     def __init__(self, sess, vec_dim, nclasses, src_lr, trg_lr, src_bs,
-                 trg_bs, src_epochs, trg_epochs, orthogonal):
+                 trg_bs, src_epochs, trg_epochs, orthogonal, summaries_dir,
+                 W_trg):
         self.sess = sess
         self.vec_dim = vec_dim
         self.nclasses = nclasses
@@ -20,6 +25,8 @@ class BiSentiLR(object):
         self.src_epochs = src_epochs
         self.trg_epochs = trg_epochs
         self.orthogonal = orthogonal
+        self.summaries_dir = summaries_dir
+        self.W_trg = W_trg
         self._build_graph()
         self.sess.run(tf.global_variables_initializer())
 
@@ -28,12 +35,15 @@ class BiSentiLR(object):
             with tf.variable_scope('softmax', reuse=tf.AUTO_REUSE):
                 W2 = tf.get_variable('W2', (self.vec_dim, self.nclasses), tf.float32, initializer=tf.random_uniform_initializer(-1., 1.))
                 b2 = tf.get_variable('b2', (self.nclasses,), tf.float32, initializer=tf.zeros_initializer())
+                common_summ.append(tf.summary.histogram('W2', W2))
+                common_summ.append(tf.summary.histogram('b2', b2))
             return inputs @ W2 + b2
 
         def project(inputs):
             with tf.variable_scope('project', reuse=tf.AUTO_REUSE):
-                U = tf.get_variable('U', (self.vec_dim, self.vec_dim), tf.float32, initializer=tf.constant_initializer(np.identity(self.vec_dim)))
+                U = tf.get_variable('U', (self.vec_dim, self.vec_dim), tf.float32, initializer=(tf.random_uniform_initializer(-1., 1.) if self.W_trg is None else tf.constant_initializer(self.W_trg)))
                 self.U = U
+                trg_summ.append(tf.summary.histogram('U', U))
                 return inputs @ U
 
         self.src_x = tf.placeholder(tf.float32, shape=(None, self.vec_dim))
@@ -42,6 +52,10 @@ class BiSentiLR(object):
         self.trg_y = tf.placeholder(tf.int32, shape=(None,))
         self.X_src = tf.placeholder(tf.float32, shape=(None, self.vec_dim))
         self.X_trg = tf.placeholder(tf.float32, shape=(None, self.vec_dim))
+
+        src_summ = []
+        trg_summ = []
+        common_summ = []
 
         src_logits = softmax(self.src_x)
         trg_logits = softmax(project(self.trg_x))
@@ -54,8 +68,14 @@ class BiSentiLR(object):
 
         proj_loss = tf.reduce_sum(tf.squared_difference(self.X_src, project(self.X_trg)))
 
+        src_summ.append(tf.summary.scalar('src_loss', src_loss))
+        trg_summ.append(tf.summary.scalar('trg_loss', trg_loss))
+
+        self.src_merged = tf.summary.merge(src_summ + common_summ)
+        self.trg_merged = tf.summary.merge(trg_summ + common_summ)
+
         sx, ux, vx = tf.svd(self.U)
-        U_ortho = self.U.assign(tf.matmul(ux, vx, transpose_b=True))
+        U_ortho = self.U.assign(tf.matmul(ux, vx, adjoint_b=True))
 
         self.proj_loss = proj_loss
         self.src_loss = src_loss
@@ -67,8 +87,12 @@ class BiSentiLR(object):
         self.U_ortho = U_ortho
 
     def fit(self, src_x, src_y, trg_x, trg_y, src_val_x=None, src_val_y=None, trg_val_x=None, trg_val_y=None, X_src=None, X_trg=None):
+        src_writer = tf.summary.FileWriter(self.summaries_dir + '/src', self.sess.graph)
+        trg_writer = tf.summary.FileWriter(self.summaries_dir + '/trg', self.sess.graph)
+
         # train W1, W2, b using src_x and src_y
         nsample = len(src_x)
+        cnt = 0
         for epoch in range(self.src_epochs):
             src_loss = 0.
             src_pred = np.zeros(nsample)
@@ -78,9 +102,13 @@ class BiSentiLR(object):
                     self.src_x: src_x[i:j],
                     self.src_y: src_y[i:j],
                 }
-                _, src_loss_, src_pred_ = self.sess.run([self.src_op, self.src_loss, self.src_pred], feed_dict)
+                _, src_loss_, src_pred_, src_merged_ = self.sess.run([self.src_op, self.src_loss, self.src_pred, self.src_merged], feed_dict)
                 src_loss += src_loss_ * (j - i)
                 src_pred[i:j] = src_pred_
+
+                src_writer.add_summary(src_merged_, cnt)
+                cnt += 1
+
             src_loss /= nsample
             fscore = f1_score(src_y, src_pred, average='macro')
             logging.info('epoch: %d  f1_macro: %.4f  loss: %.6f' % (epoch, fscore, src_loss))
@@ -91,8 +119,10 @@ class BiSentiLR(object):
         logging.info('==========================================================')
         logging.info('                  Start training W_target                 ')
         logging.info('==========================================================')
+
         # train U using trg_x and trg_y
         nsample = len(trg_x)
+        cnt = 0
         for epoch in range(self.trg_epochs):
             trg_loss = 0.
             trg_pred = np.zeros(nsample)
@@ -102,14 +132,18 @@ class BiSentiLR(object):
                     self.trg_x: trg_x[i:j],
                     self.trg_y: trg_y[i:j],
                 }
-                _, trg_loss_, trg_pred_, U_ = self.sess.run([self.trg_op, self.trg_loss, self.trg_pred, self.U], feed_dict)
+                _, trg_loss_, trg_pred_, U_, trg_merged_ = self.sess.run([self.trg_op, self.trg_loss, self.trg_pred, self.U, self.trg_merged], feed_dict)
                 trg_loss += trg_loss_ * (j - i)
                 trg_pred[i:j] = trg_pred_
-                logging.info(str(U_[0, 0]))
+
+                trg_writer.add_summary(trg_merged_, cnt)
+                cnt += 1
+#                 DEBUG('trg_loss_: %.8f' % trg_loss_)
+#                 DEBUG(U_[:2, :2])
 
             if self.orthogonal:
                 _, U_ = self.sess.run([self.U_ortho, self.U])
-                logging.info(str(U_[0, 0]))
+#                 DEBUG(U_[:2, :2])
 
             trg_loss /= nsample
             fscore = f1_score(trg_y, trg_pred, average='macro')
@@ -123,7 +157,7 @@ class BiSentiLR(object):
                 logging.info('Projection error: %.4f' % proj_loss_)
 
         logging.info('==========================================================')
-        logging.info('Test f1_macro: %.4f' % self.score(src_val_x, src_val_y))
+        DEBUG('Test f1_macro: %.4f' % self.score(src_val_x, src_val_y))
 
     def predict(self, test_x):
         pred = self.sess.run(self.src_pred, {self.src_x: test_x})
@@ -156,7 +190,10 @@ class BiSentiLR(object):
 def make_data(X, y, embedding, vec_dim, binary, pad_id, shuffle=True):
     X_new = np.zeros((len(X), vec_dim), dtype=np.float32)
     for i, row in enumerate(X):
-        X_new[i] = np.mean(embedding[row], axis=0)
+        if len(row) > 0:
+            X_new[i] = np.mean(embedding[row], axis=0)
+        else:
+            logging.warning('ZERO LENGTH EXAMPLE')
     X = X_new
     if shuffle:
         perm = np.random.permutation(X.shape[0])
@@ -183,12 +220,16 @@ def main(args):
     X_src = src_wv.embedding[gold_dict[:, 0]]
     X_trg = trg_wv.embedding[gold_dict[:, 1]]
 
+#     u, s, vt = np.linalg.svd(np.dot(X_src.T, X_trg))
+#     W_trg = np.dot(vt.T, u.T)
+    W_trg = None
+
     with tf.Session() as sess:
         model = BiSentiLR(sess=sess, vec_dim=args.vector_dim, nclasses=(2 if args.binary else 4),
-                           src_lr=args.source_learning_rate, trg_lr=args.target_learning_rate,
-                           src_bs=args.source_batch_size, trg_bs=args.target_batch_size,
-                           src_epochs=args.source_epochs, trg_epochs=args.target_epochs,
-                           orthogonal=args.orthogonal)
+                          src_lr=args.source_learning_rate, trg_lr=args.target_learning_rate,
+                          src_bs=args.source_batch_size, trg_bs=args.target_batch_size,
+                          src_epochs=args.source_epochs, trg_epochs=args.target_epochs,
+                          orthogonal=args.orthogonal, summaries_dir=args.summaries_dir, W_trg=W_trg)
         model.fit(src_x, src_y, trg_x, trg_y, src_test_x, src_test_y, trg_test_x, trg_test_y, X_src, X_trg)
         model.save(args.save_path)
 
@@ -204,11 +245,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-bi', '--binary', action='store_true', help='use 2-class set up')
     parser.add_argument('-slr', '--source_learning_rate', type=float, default=0.001, help='learning rate (default: 0.001)')
-    parser.add_argument('-tlr', '--target_learning_rate', type=float, default=0.000000001, help='learning rate (default: 0.000001)')
+    parser.add_argument('-tlr', '--target_learning_rate', type=float, default=0.1, help='learning rate (default: 0.1)')
     parser.add_argument('-sep', '--source_epochs', default=200, type=int, help='training epochs (default: 200)')
     parser.add_argument('-tep', '--target_epochs', default=200, type=int, help='training epochs (default: 200)')
     parser.add_argument('-sbs', '--source_batch_size', default=50, type=int, help='training batch size (default: 50)')
-    parser.add_argument('-tbs', '--target_batch_size', default=50, type=int, help='training batch size (default: 50)')
+    parser.add_argument('-tbs', '--target_batch_size', default=500, type=int, help='training batch size (default: 500)')
     parser.add_argument('-se', '--source_embedding', default='./emb/en.bin', help='source embedding (default: ./emb/en.bin)')
     parser.add_argument('-te', '--target_embedding', default='./emb/es.bin', help='target embedding (default: ./emb/es.bin)')
     parser.add_argument('-sd', '--source_dataset', default='./datasets/en/opener_sents/', help='sentiment dataset of the source language')
@@ -219,9 +260,11 @@ if __name__ == '__main__':
     parser.add_argument('--orthogonal', action='store_true', help='restrict W_target to be orthogonal during training')
     parser.add_argument('--cuda', action='store_true', help='use cuda for BDI')
     parser.add_argument('--save_path', type=str, default='./checkpoints/bilr.ckpt', help='file to save the trained parameters')
+    parser.add_argument('--summaries_dir', type=str, default='./log', help='dir to save summaries')
     parser.add_argument('--debug', action='store_const', dest='loglevel', default=logging.INFO, const=logging.DEBUG, help='print debug info')
 
-    parser.set_defaults(normalize=True, orthogonal=True, binary=True)
+    parser.set_defaults(normalize=True, orthogonal=False, binary=True, cuda=True)
+#     parser.set_defaults(normalize=True, orthogonal=True, binary=True, cuda=True)
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel,
                         format='%(asctime)s: %(message)s')
