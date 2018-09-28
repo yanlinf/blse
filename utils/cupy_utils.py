ab798d74845682e5586a774df7e56a7986a46ca0
evanfeng <evanfeng@pku.edu.cn>
2018-09-28 04:33:25 +0000

    get_array_module with *args **kwargs

--- utils/cupy_utils.py
+++ utils/cupy_utils.py
@@ -10,8 +10,8 @@ def get_cupy():
     return cupy
 
 
-def get_array_module(x):
+def get_array_module(*args, **kwargs):
     if cupy is not None:
-        return cupy.get_array_module(x)
+        return cupy.get_array_module(*args, **kwargs)
     else:
         return numpy
