a809bdfdeb857f7cac541b54a0713c86bad25bc3
evanfeng <evanfeng@pku.edu.cn>
2018-10-02 14:25:37 +0000

    remove get_unsupervised_dict and get_W_target

--- bimap.py
+++ bimap.py
@@ -24,33 +24,6 @@ def plot(files, labels):
     ax.set_ylabel('accuracy')
 
 
-def get_unsupervised_init_dict(src_emb, trg_emb, vocab_size, num_neighbours, norm_actions, direction):
-    sim_size = min(src_emb.shape[0], trg_emb.shape[0], vocab_size) if vocab_size > 0 else min(src_emb.shape[0], trg_emb.shape[0])
-    u, s, vt = xp.linalg.svd(src_emb[:sim_size], full_matrices=False)
-    src_sim = (u * s) @ u.T
-    u, s, vt = xp.linalg.svd(trg_emb[:sim_size], full_matrices=False)
-    trg_sim = (u * s) @ u.T
-    del u, s, vt
-
-    src_sim.sort(axis=1)
-    trg_sim.sort(axis=1)
-    utils.normalize(src_sim, norm_actions)
-    utils.normalize(trg_sim, norm_actions)
-    sim = xp.dot(src_sim, trg_sim.T)
-    del src_sim, trg_sim
-    src_knn_sim = utils.top_k_mean(sim, num_neighbours, inplace=False)
-    trg_knn_sim = utils.top_k_mean(sim.T, num_neighbours, inplace=False)
-    sim -= src_knn_sim[:, xp.newaxis] / 2 + trg_knn_sim / 2
-
-    if args.direction == 'forward':
-        init_dict = xp.stack([xp.arange(sim_size), xp.argmax(sim, axis=1)], axis=1)
-    elif args.direction == 'backward':
-        init_dict = xp.stack([xp.argmax(sim, axis=0), xp.arange(sim_size)], axis=1)
-    elif args.direction == 'union':
-        init_dict = xp.stack([xp.concatenate((xp.arange(sim_size), xp.argmax(sim, axis=0))), xp.concatenate((xp.argmax(sim, axis=1), xp.arange(sim_size)))], axis=1)
-    return init_dict
-
-
 def get_numeral_init_dict(src_wv, trg_wv):
     num_regex = re.compile('^[0-9]+$')
     src_nums = {w for w in src_wv.vocab if num_regex.match(w) is not None}
@@ -58,16 +31,6 @@ def get_numeral_init_dict(src_wv, trg_wv):
     common = src_nums & trg_nums
     init_dict = xp.array([[src_wv.word2index(w), trg_wv.word2index(w)] for w in common], dtype=xp.int32)
     return init_dict
-
-
-def get_W_target(X_src, X_trg, orthogonal, out=None):
-    if orthogonal:
-        u, s, vt = xp.linalg.svd(xp.dot(X_src.T, X_trg))
-        W_trg = xp.dot(vt.T, u.T, out=out)
-    else:
-        W_trg = xp.dot(xp.linalg.pinv(X_trg), X_src, out=out)
-    if out is None:
-        return W_trg
 
 
 def main(args):
@@ -89,11 +52,11 @@ def main(args):
     trg_emb = xp.array(trg_wv.embedding, dtype=xp.float32)
     gold_dict = xp.array(utils.BilingualDict(args.gold_dictionary).get_indexed_dictionary(src_wv, trg_wv), dtype=xp.int32)
     keep_prob = args.dropout_init
-
+    
     if args.init_num:
         init_dict = get_numeral_init_dict(src_wv, trg_wv)
     elif args.init_unsupervised:
-        init_dict = get_unsupervised_init_dict(src_emb, trg_emb, args.vocab_cutoff, args.csls, args.normalize, args.direction)
+        init_dict = utils.get_unsupervised_init_dict(src_emb, trg_emb, args.vocab_cutoff, args.csls, args.normalize, args.direction)
     else:
         init_dict = xp.array(utils.BilingualDict(args.init_dictionary).get_indexed_dictionary(src_wv, trg_wv), dtype=xp.int32)
     curr_dict = init_dict
@@ -110,20 +73,20 @@ def main(args):
             with open(args.W_target, 'rb') as fin:
                 W_trg = pickle.load(fin)
         else:
-            W_trg = get_W_target(X_src, X_trg, orthogonal=args.orthogonal)
+            W_trg = utils.get_projection_matrix(X_src, X_trg, orthogonal=args.orthogonal, direction='backward')
         bdi_obj.project(W_trg)
 
         # dictionary induction
         curr_dict = bdi_obj.get_bilingual_dict_with_cutoff(keep_prob=keep_prob)
-        
+
         # update keep_prob
         if (epoch + 1) % args.dropout_interval == 0:
             keep_prob = min(1., keep_prob + args.dropout_step)
 
         # valiadation
         if not args.no_valiadation and (epoch + 1) % args.valiadation_step == 0 or epoch == (args.epochs - 1):
-            val_trg_ind = bdi_obj.get_target_indices(gold_dict[:,0])
-            accuracy = xp.mean((val_trg_ind == gold_dict[:,1]).astype(xp.int32))
+            val_trg_ind = bdi_obj.get_target_indices(gold_dict[:, 0])
+            accuracy = xp.mean((val_trg_ind == gold_dict[:, 1]).astype(xp.int32))
             logging.info('epoch: %d   accuracy: %.4f   dict_size: %d' % (epoch, accuracy, curr_dict.shape[0]))
             log_file.write('%d,%.4f\n' % (epoch, accuracy))
 
@@ -155,7 +118,7 @@ if __name__ == '__main__':
     parser.add_argument('--cuda', action='store_true', help='use cuda to accelerate')
     parser.add_argument('--log', default='./log/init100.csv', type=str, help='file to print log')
     parser.add_argument('--plot', action='store_true', help='plot results')
-    
+
     init_group = parser.add_mutually_exclusive_group()
     init_group.add_argument('-d', '--init_dictionary', default='./init_dict/init100.txt', help='bilingual dictionary for learning bilingual mapping (default: ./init_dict/init100.txt)')
     init_group.add_argument('--init_num', action='store_true', help='use numerals as initial dictionary')
@@ -167,8 +130,6 @@ if __name__ == '__main__':
 
     induction_group = parser.add_argument_group()
     induction_group.add_argument('-vc', '--vocab_cutoff', default=10000, type=int, help='restrict the vocabulary to k most frequent words')
-    induction_group.add_argument('-tc', '--target_cutoff', action='store_true', help='target vocab cutoff during training')
-    induction_group.add_argument('-sc', '--source_cutoff', action='store_true', help='source vocab cutoff during training')
     induction_group.add_argument('--csls', type=int, default=10, help='number of csls neighbours')
     induction_group.add_argument('--dropout_init', type=float, default=0.1, help='initial keep prob of the dropout machanism')
     induction_group.add_argument('--dropout_interval', type=int, default=30, help='increase keep_prob every m steps')
@@ -176,17 +137,22 @@ if __name__ == '__main__':
     induction_group.add_argument('--direction', choices=['forward', 'backward', 'union'], default='union', help='direction of dictionary induction')
 
     recommend_group = parser.add_mutually_exclusive_group()
-    recommend_group.add_argument('-u', '--unsupervised', action='store_true', help='use recommended settings')
+    recommend_group.add_argument('-u', '--unsupervised', action='store_true', help='use unsupervised settings')
+    recommend_group.add_argument('-uc', '--unconstrained', action='store_true', help='use unsupervised and unconstrained settings')
     recommend_group.add_argument('-s5', '--supervised5000', action='store_true', help='use supervised5000 settings')
     recommend_group.add_argument('-s1', '--supervised100', action='store_true', help='use supervised100 settings')
 
     args = parser.parse_args()
     if args.unsupervised:
-        parser.set_defaults(init_unsupervised=True, target_cutoff=True, source_cutoff=True, csls=10, direction='union', cuda=True, normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=True, log='./log/unsupervised.csv')
+        parser.set_defaults(init_unsupervised=True, csls=10, direction='union', cuda=True, normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=True, log='./log/unsupervised.csv')
+    elif args.unconstrained:
+        parser.set_defaults(init_unsupervised=True, csls=10, direction='union', cuda=True, normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=False, log='./log/unsupervised.csv')
     elif args.supervised5000:
-        parser.set_defaults(init_dictionary='./init_dict/init5000.txt', target_cutoff=True, source_cutoff=True, csls=10, direction='union', cuda=True, normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=True, log='./log/supervised5000.csv')
+        parser.set_defaults(init_dictionary='./init_dict/init5000.txt', csls=10, direction='union', cuda=True,
+                            normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=True, log='./log/supervised5000.csv')
     elif args.supervised100:
-        parser.set_defaults(init_dictionary='./init_dict/init100.txt', target_cutoff=True, source_cutoff=True, csls=10, direction='union', cuda=True, normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=True, log='./log/supervised100.csv')
+        parser.set_defaults(init_dictionary='./init_dict/init100.txt', csls=10, direction='union', cuda=True,
+                            normalize=['center', 'unit'], vocab_cutoff=10000, orthogonal=True, log='./log/supervised100.csv')
 
     args = parser.parse_args()
 
