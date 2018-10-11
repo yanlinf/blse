625280dd905a64e9b90a5a9d78235a677d67f38e
evanfeng <evanfeng@pku.edu.cn>
2018-10-11 14:07:16 +0000

    download fasttext wiki vectors

---
+++ download_fasttext_wiki.sh
@@ -0,0 +1,4 @@
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.vec
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.es.vec
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.ca.vec
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.eu.vec
