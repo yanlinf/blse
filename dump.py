fea30d3269a7ddc9baedf3396b89938ba9687886
evanfeng <evanfeng@pku.edu.cn>
2018-10-12 16:31:55 +0000

    pickle word embedding for faster loading

---
+++ dump.py
@@ -0,0 +1,13 @@
+import pickle
+from utils.utils import *
+
+
+def main():
+    for lang in ('eu', 'en', 'es', 'ca'):
+        wv = WordVecs('emb/wiki.%s.vec' % lang, emb_format='fasttext_text').normalize(['center', 'unit'])
+        with open('pickle/%s.bin' % lang, 'wb') as fout:
+            pickle.dump(wv, fout)
+
+    
+if __name__ == '__main__':
+    main()
