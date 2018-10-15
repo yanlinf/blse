3718b6bb31028e4f0ccec441c26ac63ff578911c
evanfeng <evanfeng@pku.edu.cn>
2018-10-15 23:30:00 +0800

    add --no-check-certificate

--- download_fasttext_wiki.sh
+++ download_fasttext_wiki.sh
@@ -1,4 +1,4 @@
-wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.vec
-wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.es.vec
-wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.ca.vec
-wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.eu.vec
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.vec --no-check-certificate
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.es.vec --no-check-certificate
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.ca.vec --no-check-certificate
+wget -P emb/ https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.eu.vec --no-check-certificate
