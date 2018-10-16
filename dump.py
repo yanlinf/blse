3a03e4ec0f5861f1752723e9a61e0cf20c3c043b
evanfeng <evanfeng@pku.edu.cn>
2018-10-16 14:26:59 +0800

    fix path not exist bug

--- dump.py
+++ dump.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 from utils.dataset import *
 from utils.math import *
 from utils.bdi import *
@@ -6,6 +7,8 @@ from utils.bdi import *
 
 def main():
     for lang in ('eu', 'en', 'es', 'ca'):
+        if not os.path.exists('pickle'):
+            os.mkdir('pickle')
         wv = WordVecs('emb/wiki.%s.vec' % lang, emb_format='fasttext_text').normalize(['center', 'unit'])
         with open('pickle/%s.bin' % lang, 'wb') as fout:
             pickle.dump(wv, fout)
