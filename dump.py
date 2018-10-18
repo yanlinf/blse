484fff2d941649f704e682e9def719f7108ddc27
evanfeng <evanfeng@pku.edu.cn>
2018-10-18 23:02:34 +0800

    add TARGET FORMAT

--- dump.py
+++ dump.py
@@ -4,15 +4,18 @@ from utils.dataset import *
 from utils.math import *
 from utils.bdi import *
 
+TARGET = 'emb/%s.bin'
+FORMAT = 'word2vec_bin'
+
 
 def main():
+    if not os.path.exists('pickle'):
+        os.mkdir('pickle')
     for lang in ('eu', 'en', 'es', 'ca'):
-        if not os.path.exists('pickle'):
-            os.mkdir('pickle')
-        wv = WordVecs('emb/wiki.%s.vec' % lang, emb_format='fasttext_text').normalize(['center', 'unit'])
+        wv = WordVecs(TARGET % lang, emb_format=FORMAT).normalize(['center', 'unit'])
         with open('pickle/%s.bin' % lang, 'wb') as fout:
             pickle.dump(wv, fout)
 
-    
+
 if __name__ == '__main__':
     main()
