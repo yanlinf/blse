e353ce3850c4a665aa526e3d746b008885a4c427
evanfeng <evanfeng@pku.edu.cn>
2018-10-18 23:03:42 +0800

    generate identity matrix

---
+++ gen_identity.py
@@ -0,0 +1,27 @@
+import pickle
+import os
+import numpy as np
+from utils.dataset import *
+from utils.math import *
+from utils.bdi import *
+
+TARGET = 'checkpoints/en-%s-idenity'
+
+
+def main():
+    if not os.path.exists('pickle'):
+        os.mkdir('pickle')
+    for lang in ('eu', 'es', 'ca'):
+        dic = {
+            'W_source': np.identity(300, dtype=np.float32),
+            'W_target': np.identity(300, dtype=np.float32),
+            'source_lang': 'en',
+            'target_lang': lang,
+            'model': 'ubi',
+        }
+        with open(TARGET % lang, 'wb') as fout:
+            pickle.dump(dic, fout)
+
+
+if __name__ == '__main__':
+    main()
