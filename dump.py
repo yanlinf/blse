d4760421f1c1253877b2e36c1cc29aadd72a1dc0
evanfeng <evanfeng@pku.edu.cn>
2018-11-10 16:23:44 +0800

    fix normalize action

--- dump.py
+++ dump.py
@@ -6,13 +6,13 @@ from utils.bdi import *
 
 TARGET = 'emb/wiki.%s.vec'
 FORMAT = 'fasttext_text'
-
+NORMALIZE = ('unit',)
 
 def main():
     if not os.path.exists('pickle'):
         os.mkdir('pickle')
     for lang in ('eu', 'en', 'es', 'ca'):
-        wv = WordVecs(TARGET % lang, emb_format=FORMAT).normalize(['center', 'unit'])
+        wv = WordVecs(TARGET % lang, emb_format=FORMAT).normalize(NORMALIZE)
         with open('pickle/%s.bin' % lang, 'wb') as fout:
             pickle.dump(wv, fout)
 
