c02ae927b9394143b23d602296d2383aa09443ab
evanfeng <evanfeng@pku.edu.cn>
2018-11-23 00:17:56 +0800

    fix params

--- run_bimap.sh
+++ run_bimap.sh
@@ -1,7 +1,7 @@
-for i in `seq 5`
+for i in 1
 do
     for lang in es ca eu
     do
-        python bimap.py --en_$lang --pickle -u --save_path checkpoints/en-$lang-ubi-$i.bin
+        python bimap.py --orthogonal --en_$lang  --dropout_init 0.1 --dropout_interval 10 --dropout_step 0.1 -e 100 --pickle -u --valiadation_step 1 --save_path checkpoints/en-$lang-bimap-$i.bin --debug
     done
 done
