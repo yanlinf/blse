62ea2e1e932d50b720f6eb9e50b0179f3ebad8a0
evanfeng <evanfeng@pku.edu.cn>
2018-11-23 00:20:24 +0800

    fix params

--- run_blse.sh
+++ run_blse.sh
@@ -1,8 +1,9 @@
-for a in 0.0001 0.001 0.01 0.1
+for a in 0.0001 0.0003 0.001 0.003 0.01 0.03 0.1 0.3 0.9
 do
     for lang in es ca eu
     do
-        python blse.py --en_$lang --pickle -bi --save_path checkpoints/en-$lang-blse-a$a-bi.bin
-        python blse.py --en_$lang --pickle --save_path checkpoints/en-$lang-blse-a$a.bin
+        python blse.py --en_$lang -lr 0.01 -a $a --pickle -bi --save_path checkpoints/en-$lang-blse-a$a-bi.bin
+        python blse.py --en_$lang -lr 0.01 -a $a --pickle --save_path checkpoints/en-$lang-blse-a$a.bin
     done
 done
+python cnn_eval.py checkpoints/*blse* -o log/blse-cnn.csv
