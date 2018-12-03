42edccadee314ff5359a4423a2f4cfe49f3834de
evanfeng <evanfeng@pku.edu.cn>
2018-12-03 18:30:31 +0800

    fix search_range, langs and eval

--- run_blse.sh
+++ run_blse.sh
@@ -1,9 +1,10 @@
-for a in 0.0001 0.0003 0.001 0.003 0.01 0.03 0.1 0.3 0.9
+for a in 0.0001 0.001 0.01 0.1 0.3 0.5 0.7 0.9
 do
     for lang in es ca eu
     do
-        python blse.py --en_$lang -lr 0.01 -a $a --pickle -bi --save_path checkpoints/en-$lang-blse-a$a-bi.bin
-        python blse.py --en_$lang -lr 0.01 -a $a --pickle --save_path checkpoints/en-$lang-blse-a$a.bin
+        python -u blse.py --en_$lang -lr 0.01 -a $a --pickle -bi --save_path checkpoints/en-$lang-blse-a$a-bi.bin
+        python -u blse.py --en_$lang -lr 0.01 -a $a --pickle --save_path checkpoints/en-$lang-blse-a$a.bin
     done
 done
-python cnn_eval.py checkpoints/*blse* -o log/blse-cnn.csv
+python -u convert.py checkpoints/en-??-blse* ubise
+python -u dan_eval.py checkpoints/en-??-blse* -o log/blse-1203-dan.csv
