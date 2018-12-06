426276bcdb64ed00dca274ded4b36856307d1dad
evanfeng <evanfeng@pku.edu.cn>
2018-12-06 16:22:37 +0800

    remove jr de ja

--- run_artetxe.sh
+++ run_artetxe.sh
@@ -1,4 +1,4 @@
-for lang in es ca eu fr de ja
+for lang in es ca eu
 do
     python artetxe.py --en_$lang --project_source --orthogonal --save_path checkpoints/en-$lang-artetxe-s-o.bin --pickle
     python artetxe.py --en_$lang --orthogonal --save_path checkpoints/en-$lang-artetxe-t-o.bin --pickle
